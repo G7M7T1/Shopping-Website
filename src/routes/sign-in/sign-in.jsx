fe87291607e827ccf2c23914f0490035937f5b51
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-06-13 22:18:48 -0400

    Other File

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,9 +1,18 @@
+import {signInWithGooglePopup, createUserProfileDocument} from "../../utils/firebase/firebase"
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        // await createUserProfileDocument(response);
+        console.log(response)
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
         </div>
-    )
-}
+    );
+};
 
 export default SignIn
